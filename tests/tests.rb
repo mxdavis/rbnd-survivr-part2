1316e427ecca74636eaac53db54071e8d468955c
Malki Davis <md01@me.com>
2016-08-29 12:41:36 +0300

    add class names to all files

--- tests/tests.rb
+++ tests/tests.rb
@@ -3,16 +3,16 @@
 
 #1
 require_relative "contestant_test" #once all the tests here pass, uncomment the next line, and so on.
-#
-# #2
+
+#2
 # require_relative "jury_test"
-#
+
 # #3
 # require_relative "tribe_test"
-#
+
 # #4
 # require_relative "game_test"
-#
+
 # #5
 # require_relative "survivr_test"
 
