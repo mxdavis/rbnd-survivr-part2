556ee4ffc52c718fbb0c55ce7aa7e5575460b05e
Malki Davis <md01@me.com>
2016-08-29 23:42:40 +0300

    work on first half of game tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,22 @@
+require_relative "../lib/tribe"
+require_relative "../lib/contestant"
+
 class Game
+  attr_accessor :tribes
+
+  def initialize(tribe1, tribe2)
+  	@tribes = [tribe1, tribe2]
+  end
+
+  def add_tribe(tribe)
+  	@tribes << tribe
+  end
+
+  def immunity_challenge
+  	@tribes.sample
+  end 
+
+  def clear_tribes
+  	@tribes.clear
+  end
 end
