a21db049152740453bf3c6a21fa69b4e222a66b0
Malki Davis <md01@me.com>
2016-08-30 00:41:02 +0300

    first phase finished

--- lib/survivr.rb
+++ lib/survivr.rb
@@ -6,23 +6,31 @@ require_relative "jury"
 #After your tests pass, uncomment this code below
 #=========================================================
 # # Create an array of twenty hopefuls to compete on the island of Borneo
-# @contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
-# @contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
-#
+@contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
+@contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
+
 # # Create two new tribes with names
-# @coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
-# @hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
+@coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
+@hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
 #
 # # Create a new game of Survivor
-# @borneo = Game.new(@coyopa, @hunapu)
+@borneo = Game.new(@coyopa, @hunapu)
 #=========================================================
 
 
 #This is where you will write your code for the three phases
 def phase_one
+  8.times do
+  	the_loosing_team = @borneo.immunity_challenge
+  	loser = the_loosing_team.tribal_council
+  	puts "#{the_loosing_team} has lost their teammate #{loser}"
+  	the_loosing_team.members.delete(loser)
+  end
 end
 
 def phase_two
+  3.times do
+  end
 end
 
 def phase_three
@@ -31,7 +39,7 @@ end
 
 # If all the tests pass, the code below should run the entire simulation!!
 #=========================================================
-# phase_one #8 eliminations
+phase_one #8 eliminations
 # @merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
 # phase_two #3 more eliminations
 # @jury = Jury.new
