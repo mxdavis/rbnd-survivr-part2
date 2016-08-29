7deb7245c982129f2211e6c4cb5436f2a5683819
Malki Davis <md01@me.com>
2016-08-29 13:31:32 +0300

    past two first tests of jury

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,2 +1,19 @@
 class Jury
+
+  @members = []
+
+  attr_reader :members
+
+  def initialize
+    @members = []
+  end
+
+  def add_member(member)
+  	@members.unshift(member)
+  end
+
+  def cast_votes(vote)
+    @votes = {}
+  end
+
 end
